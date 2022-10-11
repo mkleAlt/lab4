59542da3c31dac92d8811f97337f5b6044724387
Marcin Kleczewski <alabamamidomama@gmail.com>
2022-10-11 15:13:37 +0200

    add tests

--- src/main.java
+++ src/main.java
@@ -1,4 +1,5 @@
 import Classes.Category;
+import Classes.Product;
 
 import java.io.*;
 import java.util.Scanner;
@@ -8,6 +9,54 @@ public class main {
         System.out.println("Zadanie 5 - testowanie klas:");
         System.out.println("\nTesty klasy Category:");
         categoryTest();
+
+        System.out.println("\nTesty klasy Product:");
+        productTest();
+    }
+
+    public static void productTest() {
+        Product prod = new Product();
+        System.out.println("getName() test: " + prod.getName());
+        System.out.print("setName() test - setting to \"null\": ");
+        try {
+            prod.setName(null);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Exception thrown - " + e.getMessage());
+        }
+        System.out.print("setName() test - setting to an empty string: ");
+        try {
+            prod.setName("");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Exception thrown - " + e.getMessage());
+        }
+        prod.setName("Test");
+        System.out.println("setName() test - setting to \"Test\": " + prod.getName());
+
+        System.out.println("getCategory() test: " + prod.getCategory());
+        System.out.print("setCategory() test - setting to \"Test\": ");
+        try {
+            prod.setCategory(new Category("Test"));
+        } catch (IllegalArgumentException e) {
+            System.out.println("Exception thrown - " + e.getMessage());
+        }
+        System.out.println(prod.getCategory());
+
+        System.out.println("getPrice() test: " + prod.getPrice());
+        System.out.print("setPrice() test - setting to 0.0: ");
+        try {
+            prod.setPrice(0.0f);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Exception thrown - " + e.getMessage());
+        }
+        System.out.print("setPrice() test - setting to 1.0: ");
+        try {
+            prod.setPrice(1.0f);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Exception thrown - " + e.getMessage());
+        }
+        System.out.println(prod.getPrice());
+
+        System.out.println("toString() test: " + prod.toString());
     }
 
     public static void categoryTest() {
