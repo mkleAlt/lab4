aa73bf969e06774d50a9d3e78653fd3ea052ca87
Marcin Kleczewski <alabamamidomama@gmail.com>
2022-11-26 17:30:41 +0100

    refactor: test for singleton pattern

--- src/main.java
+++ src/main.java
@@ -10,21 +10,27 @@ public class main {
     }
 
     public static void categoryTest() {
+        System.out.println("Test inicjalizowania kategorii metoda from: ");
         Category cat = Category.from(" ");
+        if(cat != null) {
+            System.out.println("Objekt został stworzony poprawnie");
+        } else {
+            System.out.println("Objekt nie został stworzony poprawnie");
+        }
         System.out.println("Test getterow na pustym obiekcie stworzonym przez bezargumentowy konstruktor:");
-        System.out.println("Name: " + cat.getName());
-        System.out.println("Test setterow i toString na obiekcie:");
+        System.out.println("Name: \"" + cat.getName() + "\"");
+        System.out.print("Test setterow i toString na obiekcie: ");
         cat.setName("TestName");
         System.out.println(cat.toString());
-        System.out.println("Test wyjatku dla null w konstruktorze:");
+        System.out.println("Test wyjatku dla null w setterze: ");
         try {
-            Category cat2 = Category.from(null);
+            cat.setName(null);
         } catch (IllegalArgumentException e) {
             System.out.println("Wyjatek: " + e.getMessage());
         }
-        System.out.println("Test wyjatku dla pustego stringa w konstruktorze:");
+        System.out.println("Test wyjatku dla pustego stringa w setterze:");
         try {
-            Category cat2 = Category.from("");
+            cat.setName("");
         } catch (IllegalArgumentException e) {
             System.out.println("Wyjatek: " + e.getMessage());
         }
